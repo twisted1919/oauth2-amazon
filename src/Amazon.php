597bc061091f7e283aa708bf579e6402de3504ee
twisted1919 <twisted1919@users.noreply.github.com>
2017-05-24 19:56:12 +0300

    Update Amazon.php

--- src/Amazon.php
+++ src/Amazon.php
@@ -3,12 +3,12 @@
 use League\OAuth2\Client\Entity\User;
 use League\OAuth2\Client\Provider\AbstractProvider;
 use League\OAuth2\Client\Token\AccessToken;
+use Psr\Http\Message\ResponseInterface;
 
 class Amazon extends AbstractProvider
 {
     public $scopeSeparator = ' ';
     public $testMode = false;
-    public $scopes = ['profile','payments:widget','payments:shipping_address'];
 
     public function __construct($options = [])
     {
@@ -19,35 +19,22 @@ class Amazon extends AbstractProvider
         }
     }
 
-    public function urlAuthorize()
+    public function getBaseAuthorizationUrl()
     {       
         return 'https://www.amazon.com/ap/oa';
     }
 
-    public function urlAccessToken()
+    public function getBaseAccessTokenUrl(array $params)
     {
         return ($this->testMode) ? 'https://api.sandbox.amazon.com/auth/o2/token' : 'https://api.amazon.com/auth/o2/token';
     }
 
-    public function urlUserDetails(\League\OAuth2\Client\Token\AccessToken $token)
+    public function getResourceOwnerDetailsUrl(AccessToken $token)
     {
         $url = ($this->testMode) ? 'https://api.sandbox.amazon.com/user/profile' : 'https://api.amazon.com/user/profile';
         return $url . '?access_token=' . $token;
     }
-
-    public function userDetails($response, \League\OAuth2\Client\Token\AccessToken $token)
-    {
-        $user = new User();
-
-        $user->exchangeArray([
-            'uid'   => isset($response->user_id) ? $response->user_id : null,
-            'name'  => isset($response->name) ? $response->name : null,
-            'email' => isset($response->email) ? $response->email : null
-        ]);
-
-        return $user;
-    }
-
+    
     public function userUid($response, \League\OAuth2\Client\Token\AccessToken $token)
     {
         return isset($response->user_id) ? $response->user_id : null;
@@ -62,5 +49,31 @@ class Amazon extends AbstractProvider
         }
 
         return $url;
+    }
+    
+    public function getDefaultScopes()
+    {
+        return  ['profile', 'payments:widget','payments:shipping_address'];
+    }
+
+    protected function checkResponse(ResponseInterface $response, $data)
+    {
+        if (!empty($data['error'])) {
+            $message = $data['error']['type'].': '.$data['error']['message'];
+            throw new IdentityProviderException($message, $data['error']['code'], $data);
+        }
+    }
+
+    protected function createResourceOwner(array $response, AccessToken $token)
+    {
+        $user = new User();
+
+        $user->exchangeArray([
+            'uid'   => isset($response->user_id) ? $response->user_id : null,
+            'name'  => isset($response->name) ? $response->name : null,
+            'email' => isset($response->email) ? $response->email : null
+        ]);
+
+        return $user;
     }
 }
