365c68c933a193256b7d4b9dc0a4bbb6618f361b
twisted1919 <twisted1919@users.noreply.github.com>
2017-05-24 20:12:10 +0300

    Create AmazonUser.php

---
+++ src/AmazonUser.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace League\OAuth2\Client\Provider;
+
+class AmazonUser implements ResourceOwnerInterface
+{
+    /**
+     * @var array
+     */
+    protected $data;
+
+    /**
+     * @param  array $response
+     */
+    public function __construct(array $response)
+    {
+        $this->data = $response;
+    }
+
+    /**
+     * Returns the ID for the user as a string if present.
+     *
+     * @return string|null
+     */
+    public function getId()
+    {
+        return $this->getField('uid');
+    }
+
+    /**
+     * Returns the name for the user as a string if present.
+     *
+     * @return string|null
+     */
+    public function getName()
+    {
+        return $this->getField('name');
+    }
+
+    /**
+     * Returns the first name for the user as a string if present.
+     *
+     * @return string|null
+     */
+    public function getFirstName()
+    {
+        return $this->getField('first_name');
+    }
+
+    /**
+     * Returns the last name for the user as a string if present.
+     *
+     * @return string|null
+     */
+    public function getLastName()
+    {
+        return $this->getField('last_name');
+    }
+
+    /**
+     * Returns the email for the user as a string if present.
+     *
+     * @return string|null
+     */
+    public function getEmail()
+    {
+        return $this->getField('email');
+    }
+    
+    /**
+     * Returns all the data obtained about the user.
+     *
+     * @return array
+     */
+    public function toArray()
+    {
+        return $this->data;
+    }
+
+    /**
+     * Returns a field from the Graph node data.
+     *
+     * @param string $key
+     *
+     * @return mixed|null
+     */
+    private function getField($key)
+    {
+        return isset($this->data[$key]) ? $this->data[$key] : null;
+    }
+}
